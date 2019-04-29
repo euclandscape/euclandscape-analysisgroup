82872b5460cca0cd78df8b3eb0b9db31ca39fef0
Rose Andrew <randre20@une.edu.au>
2019-04-29 17:32:20 +1000

    juvenilia

---
+++ Rose.R
@@ -0,0 +1,17 @@
+## test script
+
+greeting="Hello. My name is Rose"
+
+
+
+
+greeting
+
+
+person="Tara"
+
+gsub("."," Peter.",greeting,fixed = T)
+
+person="Tara"
+message=" This is my test script."
+paste0("Hello ", person,".", message)
